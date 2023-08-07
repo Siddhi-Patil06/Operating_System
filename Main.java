35a66bfe4493cd7f4ac9a4b20c933a4790a74412
Siddhi Patil <siddhimpatill06@gmail.com>
2023-08-07 14:17:52 +0530

    changes made to load

--- Main.java
+++ Main.java
@@ -2,19 +2,18 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        OS ubuntu = new OS("Input.txt","output.txt");
+        OS ubuntu = new OS("Inpt1.txt","output.txt");
         ubuntu.LOAD();
     }
 }
 
-public class OS {
+class OS {
 
     char [][]M = new char[100][4];
     char []buffer = new char[40];
     char []R = new char[4];
     char []IR = new char[4];
     int IC;
-    int T;
     int C;
     int SI;
 
@@ -39,20 +38,21 @@ public class OS {
     }
 
     public void LOAD() {
-        int flag = 0;
+        // int flag = 0;
         String line;
+        m = 0;
         try {
             while((line = fread.readLine()) != null) {
                 buffer = line.toCharArray();
                 if(buffer[0] == '$' && buffer[1] == 'A' && buffer[2] == 'M' && buffer[3] == 'J') {
-                    System.out.println("Program card detected.");
+                    System.out.println("Control card detected.");
                     //init();
                     continue;
                 }
                 else if(buffer[0] == '$' && buffer[1] == 'D' && buffer[2] == 'T' && buffer[3] == 'A') {
                     System.out.println("Data card detected.");
                     //execute();
-                    flag = 2;
+                    // flag = 2;
                     continue;
                 }
                 else if(buffer[0] == '$' && buffer[1] == 'E' && buffer[2] == 'N' && buffer[3] == 'D') {
@@ -66,13 +66,24 @@ public class OS {
                     System.out.println("Abort due to exceed memory usage");
                 }
                 System.out.println("Your program starts here");
-                for(int i = 0; i < line.length();) {
-                    M[m][i % 4] = buffer[i];
-                    i++;
-                    if(i % 4 == 0) {
+                for(int i = 0; i < line.length() ; i++) {
+                    if( i % 4 == 0 ) {
                         m++;
+                        // if(i!=0)
+                        // System.out.println();
                     }
+                    M[m-1][i % 4] = buffer[i];  
+                    // System.out.print(buffer[i]);
                 }
+            }
+            System.out.println("The main memory is: ");
+            for(int j = 0; j < 100; j++) {
+                System.out.print(j);                
+                System.out.print(". ");
+                for(int k = 0; k < 4; k++) {
+                    System.out.print(M[j][k]);
+                }
+                System.out.println();
             }
             output.close();
         }
