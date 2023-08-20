dd357067d9d0705b22d46655fd21fd4da7a55a6f
Rajkumar Dongre <rajkumardongre17@gmail.com>
2023-08-20 17:40:51 +0530

    Completed CP

--- Main.java
+++ Main.java
@@ -25,6 +25,25 @@ class OS {
     FileWriter output;
     BufferedWriter fwrite;
 
+    public void INIT(){
+        for(int i=0; i<100 ; i++){
+            for(int j=0; j<4; j++){
+                M[i][j] = ' ';
+            }
+        }
+        for(int i=0; i<40; i++){
+            buffer[i] = ' ';
+        }
+        for(int i=0; i<4; i++){
+            IR[i] = ' ';
+            R[i] = ' ';
+        }
+        IC = 0;
+        SI = 0;
+        C = false;
+        m = 0;
+    }
+
     public OS(String file, String output) {
         this.input_file = file;        
         this.output_file = output;
@@ -45,7 +64,6 @@ class OS {
         switch (SI) {
             case 1:
             // GD
-            // System.out.println("Inside MOS : "+SI);
                 READ();
                 break;
             case 2:
@@ -54,9 +72,7 @@ class OS {
                 break;
             case 3:
             // H
-                print_memory();
-
-                // TERMINATE();
+                TERMINATE();
                 break;
             default:
                 break;
@@ -64,28 +80,33 @@ class OS {
     }
 
     public void READ(){
-        // GD30
         IR[3] = '0';
-        int start = (IR[2]-48)*10+(IR[3]-48);
-        // System.out.println("Read : "+start);
+        int start = mem_addr(IR);
         String line;
         try {
-            while((line = fread.readLine()) != null) {
-                buffer = line.toCharArray();
-                if(buffer[0] == '$' && buffer[1] == 'E' && buffer[2] == 'N' && buffer[3] == 'D') {
-                    break;
+            if((line = fread.readLine()) != null) {
+                line = rtrim(line);
+                for(int i=0; i<line.length(); i++){
+                    buffer[i] = line.charAt(i);
                 }
-                int idx = 0;
-                for(int i = start; i < start+10 ; i++) {
-                    for(int j = 0; j < 4; j++) {
-                        if(idx < line.length()) {
+                for(int i=line.length(); i<40; i++){
+                    buffer[i] = ' ';
+                }
+                if(buffer[0] == '$' && buffer[1] == 'E' && buffer[2] == 'N' && buffer[3] == 'D') {
+                    System.out.println("ABORT : Out-Of-Data");
+                    // LOAD();
+                }else{
+                    int idx = 0;
+                    for(int i = start; i < start+10 ; i++) {
+                        for(int j = 0; j < 4; j++) {
                             M[i][j] = buffer[idx];
                             idx++;
-                        }else break;
+                        }
                     }
+                    start += 10;
+                    EXECUTEUSERPROGRAM();
                 }
-                start += 10;
-            }   
+            } 
         } catch (Exception e) {
              e.printStackTrace();
         }
@@ -93,47 +114,59 @@ class OS {
 
     public void WRITE(){
         IR[3] = '0';
-        int start = (IR[2]-48)*10+(IR[3]-48);
+        int start = mem_addr(IR);
         String t,total="";
         for(int i=0;i<10;i++)
         {
             t=new String(M[start+i]);
             total=total.concat(t);
         }
-        System.out.println("Inside Write : "+total);
-        // System.out.println(total+"In write");
         try {
             fwrite.write(total);
             fwrite.write("\n");
-            // fwrite.close();
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+        EXECUTEUSERPROGRAM();
+    }
+
+    public void TERMINATE(){
+        try {
+            fwrite.newLine();                    
+            fwrite.newLine();
+            LOAD();            
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void TERMINATE(){
-        System.exit(0);
-        
+    public int mem_addr(char[] IR){
+        return (IR[2]-48)*10+(IR[3]-48);
     }
 
     private void EXECUTEUSERPROGRAM() {
-        // System.out.println("INSIDE EXECUTEUSERPROGRAM : "+IC);
         while(IC < 100) {
             IR = M[IC];
             IC++;
             String str = String.copyValueOf(IR, 0, 2);
-            // System.out.println("string value : "+str);
             switch (str) {
                 case "LR":
-                    R = M[IR[2]-48+IR[3]-48];
+                    R[0] = M[mem_addr(IR)][0];
+                    R[1] = M[mem_addr(IR)][1];
+                    R[2] = M[mem_addr(IR)][2];
+                    R[3] = M[mem_addr(IR)][3];
                     break;
 
                 case "SR":
-                    M[IR[2]-48+IR[3]-48] = R;
+                    M[mem_addr(IR)][0] = R[0];           
+                    M[mem_addr(IR)][1] = R[1];
+                    M[mem_addr(IR)][2] = R[2];
+                    M[mem_addr(IR)][3] = R[3];
+;
                     break;
 
                 case "CR":
-                    int num = IR[2]-48+IR[3]-48;
+                    int num = mem_addr(IR);
                     if(R[0] == M[num][0] && R[1] == M[num][1] && R[2] == M[num][2] && R[3] == M[num][3]) {
                         C = true;
                     }
@@ -144,14 +177,14 @@ class OS {
 
                 case "BT":
                     if(C == true) {
-                        IC = IR[2]-48+IR[3]-48;
+                        IC = mem_addr(IR);
                     }
                     break;
 
                 case "GD":
-                    // System.out.println("Inside Executeuserprog : "+str);
                     SI = 1;
                     MOS();
+                    // print_memory();
                     break;
 
                 case "PD":
@@ -172,7 +205,6 @@ class OS {
 
     public void STARTEXECUTION() {
         IC = 0;
-        // System.out.println("Inside Startexecution : "+IC);
         EXECUTEUSERPROGRAM();
     }
 
@@ -188,51 +220,55 @@ class OS {
         }
     }
 
-    public void LOAD() {
-        for(int i=0; i<100 ; i++){
-            for(int j=0; j<4; j++){
-                M[i][j] = ' ';
-            }
+    public String rtrim(String s){
+        int i = s.length()-1;
+        while (i >= 0 && Character.isWhitespace(s.charAt(i))) {
+            i--;
         }
-        // int flag = 0;
+        String rtrimStr = s.substring(0,i+1);
+        return rtrimStr;
+    }
+
+    public void LOAD() {
+        INIT();
         String line;
-        m = 0;
         try {
             while((line = fread.readLine()) != null) {
-                buffer = line.toCharArray();
+                line = rtrim(line);
+                for(int i=0; i<line.length(); i++){
+                    buffer[i] = line.charAt(i);
+                }
+                for(int i=line.length(); i<40; i++){
+                    buffer[i] = ' ';
+                }
                 if(buffer[0] == '$' && buffer[1] == 'A' && buffer[2] == 'M' && buffer[3] == 'J') {
                     System.out.println("Control card detected.");
+                    // INIT();
                     continue;
                 }
                 else if(buffer[0] == '$' && buffer[1] == 'D' && buffer[2] == 'T' && buffer[3] == 'A') {
                     System.out.println("Data card detected.");
                     STARTEXECUTION();
-                    // flag = 2;
                     continue;
                 }
                 else if(buffer[0] == '$' && buffer[1] == 'E' && buffer[2] == 'N' && buffer[3] == 'D') {
                     System.out.println("End card detected.");
-                    output.write("\n\n\n");
+                    // INIT();
                     continue;
                 }
                 if(m == 100) {
                     //abort;
                     System.out.println("Abort due to exceed memory usage");
                 }
-                System.out.println("Your program starts here");
                 int idx=0;
                 for(int i = m; i < m+10 ; i++) {
                     for(int j = 0; j < 4; j++) {
-                        if(idx < line.length()) {
                             M[i][j] = buffer[idx];
                             idx++;
-                        }else break;
                     }
                 }
                 m = m + 10;
             }
-            // printing main memory
-            output.close();
             fwrite.close();
         }
         catch(IOException e) {
